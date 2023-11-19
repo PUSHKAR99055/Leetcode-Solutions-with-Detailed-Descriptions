cafef73651e930282f3591ccfc3c6958d7ec6246
Pushkar Kumar <99055pushkar@gmail.com>
2023-11-19 16:28:45 -0500

    Adding Find First and Last Position of Element on Sorted Array

--- 34. Find First and Last Position of Element in Sorted Array/code.py
+++ 34. Find First and Last Position of Element in Sorted Array/code.py
@@ -1,13 +1,29 @@
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        @cache
-        def dfs(i, target):
-            if i == len(coins) or target < 0:
-                return float("inf")
-            if target == 0:
-                return 0
-            res = dfs(i+1, target)
-            res  = min(res,1 + dfs(i, target - coins[i]))
-            return res
-        ans = dfs(0, amount)
-        return ans if ans != float("inf") else -1
+    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:   
+        stack = []
+        new_height = [-1] * len(heights)
+        res = []
+        for i in range(len(heights)):
+            while stack and heights[i] > stack[-1][1]:
+                index, value = stack.pop()
+                new_height[index] = i
+            stack.append([i,heights[i]])
+        for i, j in queries:
+            if i > j:
+                i, j = j, i
+            if i == j:
+                res.append(i)
+            elif heights[j] > heights[i]:
+                res.append(j)
+            elif new_height[i] == -1 or new_height[j] == -1:
+                res.append(-1)
+            else:
+                curr = j
+                while curr < len(heights) and new_height[curr] != -1 and heights[curr] <= heights[i]:
+                    curr = new_height[curr]
+                if heights[curr] > heights[i]:
+                    res.append(curr)
+                else:
+                    res.append(-1)
+        return res
+
